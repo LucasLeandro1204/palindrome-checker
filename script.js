c6ff253433f9f151701b2d0e0b819d78a0318926
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-06-17 22:41:27 -0300

    working

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form');
+const input = document.querySelector('form input');
+const tbody = document.querySelector('table tbody');
+
+const isPalindrome = (string) => {
+  const clean = string.replace(/[^a-zA-Z0-9]/, '');
+  const reversed = clean.split('').reverse().join('');
+
+  return clean === reversed;
+};
+
+const createTableRow = (string, flag) => {
+  const element = document.createElement('tr');
+  
+  element.innerHTML = `<td>${string}</td><td>${flag ? 'sim' : 'não'}</td>`;
+
+  return element;
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (! input.value) {
+    return;
+  }
+
+  tbody.insertBefore(createTableRow(input.value, isPalindrome(input.value)), tbody.firstChild);
+
+  input.value = '';
+});
